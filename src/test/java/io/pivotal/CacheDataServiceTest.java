58279b7cf8e0e0f110ba44454757df10a0ca5c94
Jake Zingler <jzingler@pivotal.io>
2017-06-06 07:36:49 -0700

    update test

--- src/test/java/io/pivotal/CacheDataServiceTest.java
+++ src/test/java/io/pivotal/CacheDataServiceTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.util.StopWatch;
 
+import io.pivotal.domain.Cache;
 import io.pivotal.service.ICacheDataService;
 
 @RunWith(SpringRunner.class)
@@ -21,20 +22,17 @@ public class CacheDataServiceTest {
 	public void whenGetCacheTwiceThenTotalTimeLessThanFourSecs() {
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
-		cacheDataService.getCacheTest(123L);
-		cacheDataService.getCacheTest(123L);		
+		Cache cache1 = cacheDataService.getCacheTest(123L);
+		Cache cache2 = cacheDataService.getCacheTest(123L);		
 		stopWatch.stop();
 		Assert.assertTrue(stopWatch.getTotalTimeSeconds() < 4);
+		Assert.assertEquals(cache1, cache2);
 	}
 	
 	@Test(expected=NullPointerException.class)
 	public void whenGetCacheWithNullTwiceThenNull() {
-		StopWatch stopWatch = new StopWatch();
-		stopWatch.start();
 		cacheDataService.getCacheTestWithNull(123L);
 		cacheDataService.getCacheTestWithNull(123L);		
-		stopWatch.stop();
-		Assert.assertTrue(stopWatch.getTotalTimeSeconds() < 4);
 	}
 
 }
